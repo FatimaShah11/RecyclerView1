f8ed5faf0060542090bc7f9cf50b7b0916b1871a
FatimaShah11 <ruhishah137@gmail.com>
2021-12-17 01:15:43 +0500

    RV_Created

--- app/src/main/java/com/example/recyclerview1/MainActivity.java
+++ app/src/main/java/com/example/recyclerview1/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.recyclerview1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
  String s1[],s2[];
- int Image[];
+ int Image[]={R.drawable.CPP,R.drawable.Java,R.drawable.python};
+ RecyclerView rv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        s1=getResources().getStringArray(R.array.programmingLanguages);
+        s2=getResources().getStringArray(R.array.description);
+        rv=findViewById(R.id.RV);
+        
     }
 }
