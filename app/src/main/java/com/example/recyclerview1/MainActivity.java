648537ce841a1b61da58b5946c5faa861ea796ee
FatimaShah11 <ruhishah137@gmail.com>
2021-12-17 01:59:36 +0500

    AdapterClassFuns

--- app/src/main/java/com/example/recyclerview1/MainActivity.java
+++ app/src/main/java/com/example/recyclerview1/MainActivity.java
@@ -1,21 +1,29 @@
 package com.example.recyclerview1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
  String s1[],s2[];
- int Image[]={R.drawable.CPP,R.drawable.Java,R.drawable.python};
+ int image[]={R.drawable.CPP,R.drawable.Java,R.drawable.python};
  RecyclerView rv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rv=findViewById(R.id.RV);
+
         s1=getResources().getStringArray(R.array.programmingLanguages);
         s2=getResources().getStringArray(R.array.description);
-        rv=findViewById(R.id.RV);
+
+        MyAdapter myAdapter=new MyAdapter(this,s1,s2,image);
+        rv.setAdapter(myAdapter);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+
         
     }
 }
